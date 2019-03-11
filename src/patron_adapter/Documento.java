aef1b8dde2129e4ba6524349c61e75146cb7b3b0
Ricardo Carmona <Ricardo Carmona@DESKTOP-JN0CIEN.mshome.net>
2019-03-10 23:38:52 -0500

    Patron Adapter Terminado

---
+++ src/patron_adapter/Documento.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package patron_adapter;
+
+/**
+ *
+ * @author Ricardo Carmona
+ */
+public interface Documento {
+
+    void setContenido(String contenido);
+
+    void dibuja();
+
+    void imprime();
+}
